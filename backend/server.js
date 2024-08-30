af6e0de7bcc3a1a250c0be578c4968abdb2157af
David01226 <d.gibson1029384756@gmail.com>
2024-08-30 08:05:07 +0100

    set up express router and API routes

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,24 @@
 // dotenv package attaches environment variables to the 'process' object so we can reference them
 require('dotenv').config()
-
+// import express package
 const express = require('express')
+// import routes
+const workoutRoutes = require('./routes/workouts')
 
-// express app
+
+// init express app
 const app = express()
 
 // middleware - called everytime a request is sent to the server
+app.use(express.json())
 app.use((req, res, next) => {
   console.log(req.path, req.method)
   next()
 })
 
-// routes
-app.get('/', (req, res) => {
-  res.json({mssg: 'Welcome to the app'})
-})
+// indicate where routes are coming from when landing on url endpoint
+app.use('/api/workouts', workoutRoutes)
+
 
 // listen for requests
 app.listen(process.env.PORT, () => {
