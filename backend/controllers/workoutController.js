7fd5e7eca6cd96166b636f5e1d20fa0d7518398c
David01226 <d.gibson1029384756@gmail.com>
2024-09-04 08:12:27 +0100

    set up controllers

---
+++ backend/controllers/workoutController.js
@@ -0,0 +1,92 @@
+// import workout model
+const Workout = require('../models/workoutModel')
+const mongoose = require('mongoose')
+
+// GET all workouts
+const getAllWorkouts = async (req, res) => {
+
+  // fetches all workouts
+  const workouts = await Workout.find({}).sort({createdAt: -1})
+  res.status(200).json(workouts)
+}
+ 
+// GET a single workout
+const getSingleWorkout = async (req, res) => {
+  const { id } = req.params
+
+  // check if valid id otherwise mongoose will throw an error and crash app
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({error: 'No such workout exists'})
+  }
+
+  // fetch single workout
+  const workout = await Workout.findById(id)
+  if (!workout) {
+    return res.status(404).json({error: 'No such workout exists'})
+  }
+  
+  res.status(200).json(workout)
+}
+
+// POST a new workout
+const createWorkout = async (req, res) => {
+  const {title, load, reps} = req.body 
+
+  // add doc to db
+  try {
+    const workout = await Workout.create({title, load, reps})
+    res.status(200).json(workout)
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
+}
+
+// DELETE a workout
+const deleteWorkout = async (req, res) => {
+  const { id } = req.params
+
+  // check if valid id otherwise mongoose will throw an error and crash app
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({error: 'No such workout exists'})
+  }
+
+  // fetch single workout
+  const workout = await Workout.findOneAndDelete({_id: id})
+  if (!workout) {
+    return res.status(404).json({error: 'No such workout exists'})
+  }
+  
+  res.status(200).json(workout)
+}
+
+
+// UPDATE a workout
+const updateWorkout = async (req, res) => {
+  const { id } = req.params
+
+  // check if valid id otherwise mongoose will throw an error and crash app
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({error: 'No such workout exists'})
+  }
+
+  // 
+  const workout = await Workout.findOneAndUpdate({_id: id}, {
+    ...req.body
+  })
+
+  if (!workout) {
+    return res.status(404).json({error: 'No such workout exists'})
+  }
+  
+  res.status(200).json(workout)
+}
+
+
+// export the functions
+module.exports = {
+  createWorkout,
+  getAllWorkouts,
+  getSingleWorkout,
+  deleteWorkout,
+  updateWorkout
+}
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,6 +1,12 @@
 const express = require('express')
-// import workout model
-const Workout = require('../models/workoutModel')
+
+const { 
+  getAllWorkouts,
+  getSingleWorkout,
+  createWorkout,
+  deleteWorkout,
+  updateWorkout
+} = require('../controllers/workoutController')
 
 // instance of express router
 const router = express.Router()
@@ -8,36 +14,19 @@ const router = express.Router()
 // routes
 
 // GET all workouts
-router.get('/', (req, res) => {
-  res.json({mssg: 'GET all workouts'})
-})
+router.get('/', getAllWorkouts)
  
 // GET a single workout
-router.get('/:id', (req, res) => {
-  res.json({mssg: 'GET a single workout'})
-})
+router.get('/:id', getSingleWorkout)
 
 // POST a new workout
-router.post('/', async (req, res) => {
-  const {title, load, reps} = req.body 
-
-  try {
-    const workout = await Workout.create({title, load, reps})
-    res.status(200).json(workout)
-  } catch (error) {
-    res.status(400).json({error: error.message})
-  }
-})
+router.post('/', createWorkout)
 
 // DELETE a workout
-router.delete('/:id', (req, res) => {
-  res.json({mssg: 'DELETE a workout'})
-})
+router.delete('/:id', deleteWorkout)
 
 // UPDATE a workout
-router.patch('/:id', (req, res) => {
-  res.json({mssg: 'UPDATE a workout'})
-})
+router.patch('/:id', updateWorkout)
 
 
 // export router to have access to the routes elsewhere
